85c129d1c08a8e1a25d504e7ff5d6e11133b196c
Lorenzo Federici <36625993+BillyPap3@users.noreply.github.com>
2021-03-14 14:55:09 +0100

    Fix Dataset

--- create_stroke_database_elab.pl
+++ create_stroke_database_elab.pl
@@ -32,46 +32,17 @@ a(Male,67.0,0,1,Yes,Private,Urban,228.69,36.6,formerly_smoked,1).
 
 :- ensure_loaded(stroke_database).
 
-/*
 start :-
     tell('stroke_database_ela.pl'),
     a(Gender,Age,Hypertension,Heart_disease,Ever_married,Work_type,Residence_type,Avg_glucose_level,Bmi,Smoking_status,Target),
-    write('aa('), write(Target),
-    write(','),   write(Gender),
+    write('aa('), write(Gender),
     write(','),   ( number(Age),           Age =< 30, write('0-30');
                     number(Age), Age > 30, Age =< 40, write('30-40');
                     number(Age), Age > 40, Age =< 50, write('40-50');
                     number(Age), Age > 50, Age =< 60, write('50-60');
 				    number(Age), Age > 60, Age =< 70, write('60-70');
 				    number(Age), Age > 70, Age =< 80, write('70-80');
-				    number(Age), Age > 80,            write('80')), 
-    write(','),   write(Hypertension),
-    write(','),   write(Heart_disease), 
-    write(','),   write(Ever_married), 
-    write(','),   write(Work_type),  
-    write(','),   write(Residence_type), 
-    write(','),   ( number(Avg_glucose_level),                          Avg_glucose_level =< 110, write('0-110');
-                    number(Avg_glucose_level), Avg_glucose_level > 110, Avg_glucose_level =< 119, write('110-119');
-                    number(Avg_glucose_level), Avg_glucose_level > 119, Avg_glucose_level =< 125, write('119-125');
-				    number(Avg_glucose_level), Avg_glucose_level > 125,                           write('125')),
-    write(','),   ( number(Bmi),           Bmi =< 17, write('0-18');
-                    number(Bmi), Bmi > 17, Bmi =< 25, write('18-25');
-                    number(Bmi), Bmi > 25, Bmi =< 30, write('25-30');
-				    number(Bmi), Bmi > 30,            write('30')), 
-    write(','),   write(Smoking_status), 
-        writeln(').'),
-    fail.
-
-start :- told.
-*/
-
-start :-
-    tell('stroke_database_ela.pl'),
-    a(Gender,Age,Hypertension,Heart_disease,Ever_married,Work_type,Residence_type,Avg_glucose_level,Bmi,Smoking_status,Target),
-    write('aa('), write(Gender),
-    write(','),   ( number(Age),           Age =< 18, write('bambino');
-                    number(Age), Age > 18, Age =< 70, write('adulto');
-				    number(Age), Age > 70,            write('anziano')), 
+				    number(Age), Age > 80,            write('80')),  
     write(','),   write(Hypertension),
     write(','),   write(Heart_disease), 
     write(','),   write(Ever_married), 
