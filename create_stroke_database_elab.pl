600ff28e5f0594142b6200e286704914e6fa89d5
Andrian <melnic.andry97@gmail.com>
2021-03-14 13:20:34 +0100

    FIxed database_elab & dataset creation

--- create_stroke_database_elab.pl
+++ create_stroke_database_elab.pl
@@ -68,8 +68,7 @@ start :- told.
 start :-
     tell('stroke_database_ela.pl'),
     a(Gender,Age,Hypertension,Heart_disease,Ever_married,Work_type,Residence_type,Avg_glucose_level,Bmi,Smoking_status,Target),
-    write('aa('), write(Target),
-    write(','),   write(Gender),
+    write('aa('), write(Gender),
     write(','),   ( number(Age),           Age =< 18, write('bambino');
                     number(Age), Age > 18, Age =< 70, write('adulto');
 				    number(Age), Age > 70,            write('anziano')), 
@@ -82,11 +81,13 @@ start :-
                     number(Avg_glucose_level), Avg_glucose_level > 110, Avg_glucose_level =< 119, write('111-119');
                     number(Avg_glucose_level), Avg_glucose_level > 119, Avg_glucose_level =< 125, write('120-125');
 				    number(Avg_glucose_level), Avg_glucose_level > 125,                           write('126')),
+
     write(','),   ( number(Bmi),           Bmi =< 17, write('0-18');
                     number(Bmi), Bmi > 17, Bmi =< 25, write('19-25');
                     number(Bmi), Bmi > 25, Bmi =< 30, write('26-30');
 				    number(Bmi), Bmi > 30,            write('31')), 
     write(','),   write(Smoking_status), 
+    write(','),   write(Target),
         writeln(').'),
     fail.
 
--- create_stroke_dataset.pl
+++ create_stroke_dataset.pl
@@ -2,34 +2,34 @@
 
 startt :-
     tell('stroke_dataset.pl'),
-    setof(Gender,A^B^C^D^E^F^G^H^I^L^aa(A,Gender,B,C,D,E,F,G,H,I,L),AttributesGender),
+    setof(Gender,A^B^C^D^E^F^G^H^I^L^aa(Gender,A,B,C,D,E,F,G,H,I,L),AttributesGender),
     write('a(gender,'), writeq(AttributesGender), writeln(').'),
 
-    setof(Age,A^B^C^D^E^F^G^H^I^L^aa(A,B,Age,C,D,E,F,G,H,I,L),AttributesAge),
+    setof(Age,A^B^C^D^E^F^G^H^I^L^aa(A,Age,B,C,D,E,F,G,H,I,L),AttributesAge),
     write('a(age,'), writeq(AttributesAge), writeln(').'),
 
-    setof(Hypertension,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,Hypertension,D,E,F,G,H,I,L),AttributesHypertension),
+    setof(Hypertension,A^B^C^D^E^F^G^H^I^L^aa(A,B,Hypertension,C,D,E,F,G,H,I,L),AttributesHypertension),
     write('a(hypertension,'), writeq(AttributesHypertension), writeln(').'),
 
-    setof(Heart_disease,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,Heart_disease,E,F,G,H,I,L),AttributesHeart_disease),
+    setof(Heart_disease,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,Heart_disease,D,E,F,G,H,I,L),AttributesHeart_disease),
     write('a(heart_disease,'), writeq(AttributesHeart_disease), writeln(').'),
 
-    setof(Ever_married,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,Ever_married,F,G,H,I,L),AttributesEver_married),
+    setof(Ever_married,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,Ever_married,E,F,G,H,I,L),AttributesEver_married),
     write('a(ever_married,'), writeq(AttributesEver_married), writeln(').'),
 
-    setof(Work_type,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,Work_type,G,H,I,L),AttributesWork_type),
+    setof(Work_type,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,Work_type,F,G,H,I,L),AttributesWork_type),
     write('a(work_type,'), writeq(AttributesWork_type), writeln(').'),
 
-    setof(Residence_type,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,G,Residence_type,H,I,L),AttributesResidence_type),
+    setof(Residence_type,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,Residence_type,G,H,I,L),AttributesResidence_type),
     write('a(residence_type,'), writeq(AttributesResidence_type), writeln(').'),
 
-    setof(Avg_glucose_level,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,G,H,Avg_glucose_level,I,L),AttributesAvg_glucose_level),
+    setof(Avg_glucose_level,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,G,Avg_glucose_level,H,I,L),AttributesAvg_glucose_level),
     write('a(avg_glucose_level,'), writeq(AttributesAvg_glucose_level), writeln(').'),
 
-    setof(Bmi,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,G,H,I,Bmi,L),AttributesBmi),
+    setof(Bmi,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,G,H,Bmi,I,L),AttributesBmi),
     write('a(bmi,'), writeq(AttributesBmi), writeln(').'),
 
-    setof(Smoking_status,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,G,H,I,L,Smoking_status),AttributesSmoking_status),
+    setof(Smoking_status,A^B^C^D^E^F^G^H^I^L^aa(A,B,C,D,E,F,G,H,I,Smoking_status,L),AttributesSmoking_status),
     write('a(smoking_status,'), writeq(AttributesSmoking_status), writeln(').'),
     starttt.
 
@@ -67,7 +67,7 @@ starttt :- told.
 
 starttt :-
     aa(Gender,Age,Hypertension,Heart_disease,Ever_married,Work_type,Residence_type,Avg_glucose_level,Bmi,Smoking_status,0),
-    write('e(['),
+    write('e(healthy,['),
     write('gender = '),             writeq(Gender),             write(', '),
     write('age = '),                writeq(Age),                write(', '),
     write('hypertension = '),       writeq(Hypertension),       write(', '),
@@ -77,11 +77,11 @@ starttt :-
     write('residence_type = '),     writeq(Residence_type),     write(', '),
     write('avg_glucose_level = '),  writeq(Avg_glucose_level),  write(', '),
 	write('bmi = '),                writeq(Bmi),                write(', '),
-    write('smoking_status = '),     writeq(Smoking_status),     writeln('],healthy).'),
+    write('smoking_status = '),     writeq(Smoking_status),     writeln(']).'),
     fail.
 starttt :-
     aa(Gender,Age,Hypertension,Heart_disease,Ever_married,Work_type,Residence_type,Avg_glucose_level,Bmi,Smoking_status,1),
-    write('e(['),
+    write('e(sick['),
     write('gender = '),             writeq(Gender),             write(', '),
     write('age = '),                writeq(Age),                write(', '),
     write('hypertension = '),       writeq(Hypertension),       write(', '),
@@ -91,6 +91,6 @@ starttt :-
     write('residence_type = '),     writeq(Residence_type),     write(', '),
     write('avg_glucose_level = '),  writeq(Avg_glucose_level),  write(', '),
 	write('bmi = '),                writeq(Bmi),                write(', '),
-    write('smoking_status = '),     writeq(Smoking_status),     writeln('],sick).'),
+    write('smoking_status = '),     writeq(Smoking_status),     writeln(']).'),
     fail.
 starttt :- told.
